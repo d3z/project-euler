3e138d05c572c3b27535a437ea1e627e509f38d0
Gareth Fleming <gareth@joocy.com>
2015-08-29 21:25:46 +0100

    First problem1 implementation in clojure

--- problem1/clojure/problem1.clj
+++ problem1/clojure/problem1.clj
@@ -1,2 +1,11 @@
 (defn modCheckerMaker [num]
-  #(mod % num == 0))
+  #(= 0 (mod % num)))
+
+(defn rangeCheck [r]
+  (let [mc3 (modCheckerMaker 3) mc5 (modCheckerMaker 5)]
+    (reduce + 
+            (filter 
+             #(or (mc3 %) (mc5 %))
+             (range r)))))
+
+(rangeCheck 1000)
